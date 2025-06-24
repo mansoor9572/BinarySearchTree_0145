0d5004242b48c8e68ea94ee9db751e1e7cf062c5
mansoor <t.alyamani95@gmail.com>
2025-06-24 11:05:01 +0700

    Constructor for the node class

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -2,3 +2,15 @@
 using namespace std;             // Use the standard namespace
 
 class Node                        // Define class Node representing a node in the tree
+{
+public:
+    int info;                    // Integer data to store in the node
+    Node * leftchild;             // Pointer to left child node
+    Node *righchild;              // Pointer to right child node
+
+    // Constructor for the node class
+    Node(){
+        leftchild = nullptr;      // Initialize left child to null
+        righchild= nullptr;       // Initialize right child to null
+    }
+};
