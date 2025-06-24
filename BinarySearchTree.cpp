2fd396567342e7eb47ce3e8ed3e74b253b3ddc26
mansoor <t.alyamani95@gmail.com>
2025-06-24 11:03:59 +0700

    Define class Node representing a node in the tree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,3 +1,4 @@
 #include <iostream>              // Include input/output stream library
 using namespace std;             // Use the standard namespace
 
+class Node                        // Define class Node representing a node in the tree
